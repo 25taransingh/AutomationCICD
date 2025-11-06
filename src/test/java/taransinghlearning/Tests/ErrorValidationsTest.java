9635a7f8c703a29410e454e27448c064d6b5eae6
Admin <writetotaransingh@gmail.com>
2025-11-06 11:47:39 +0530

    reconnect jenkins

--- src/test/java/taransinghlearning/Tests/ErrorValidationsTest.java
+++ src/test/java/taransinghlearning/Tests/ErrorValidationsTest.java
@@ -18,7 +18,7 @@ public class ErrorValidationsTest extends BaseTest {
 
 	@Test(groups = { "ErrorHandling" },retryAnalyzer=Retry.class )
 	public void loginErrorValidation() throws IOException {
-		// TODO Auto-generated method stub1jjjj
+		// TODO Auto-generated method stub1jjjj1223
 
 		landingPage.loginApplication("writetotaransingh@gmail.com", "Taran@@123");
 		String errorMessage = landingPage.getErrorMessege();
